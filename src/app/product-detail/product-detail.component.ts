fb2bbfcd298314609735409e767ee3a1d5fa7354
xswxsw2012 <444655032@qq.com>
2017-12-23 17:52:14 +0800

    add_comment_assembly

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Product, ProductService, Comment } from '../shared/product.service';
+
 @Component({
   selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
@@ -10,6 +11,12 @@ import { Product, ProductService, Comment } from '../shared/product.service';
 export class ProductDetailComponent implements OnInit {
   product:Product;
   comments: Comment[];
+
+  newRating:number = 5;
+  newComment:string = "";
+
+  isCommentHidden = true;
+
   constructor(private routeInfo: ActivatedRoute,
               private productService: ProductService              
   ) { }
@@ -18,6 +25,18 @@ export class ProductDetailComponent implements OnInit {
     let productId:number = this.routeInfo.snapshot.params["productId"];
     this.product = this.productService.getProduct(productId);
     this.comments = this.productService.getCommentsForProductId(productId);
+  }
+
+  addComment(){
+    let comment = new Comment(0, this.product.id, new Date().toISOString(), "someone", this.newRating, this.newComment);
+    this.comments.unshift(comment);
+
+    let sum  = this.comments.reduce((sum, comment) => sum + comment.rating, 0);
+    this.product.rating = sum / this.comments.length;
+
+    this.newComment = null;
+    this.newRating = 5;
+    this.isCommentHidden = true;
   }
 
 }
--- src/app/stars/stars.component.ts
+++ src/app/stars/stars.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
+import { Output } from '@angular/core';
+import { EventEmitter } from '@angular/core';
+import { OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-stars',
@@ -6,19 +9,36 @@ import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./stars.component.css'],
   encapsulation: ViewEncapsulation.None
 })
-export class StarsComponent implements OnInit {
+export class StarsComponent implements OnInit, OnChanges {
   
   @Input()
   private rating:number = 0;
 
+  @Output()
+  private ratingChange:EventEmitter<number> = new EventEmitter();//必须写成ratingChange才能直接用[()]进行双向绑定
+
   private stars: boolean[]; 
+
+  @Input()
+  private readonly:boolean = true;
 
   constructor() { }
 
   ngOnInit() {
+    
+  }
+
+  ngOnChanges(changes: SimpleChanges){
     this.stars = [];
     for (let i = 1; i <= 5; i++) {
       this.stars.push(i > this.rating);
+    }
+  }
+
+  clickStar(index:number){
+    if(!this.readonly){
+      this.rating = index + 1;
+      this.ratingChange.emit(this.rating);
     }
   }
 
